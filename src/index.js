af18e10cb88cdca75cc7e6f2795844c29d417cd8
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2019-04-25 17:25:40 -0500

    store y provider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+	{}, // Reducers
+	{}, // Estado inicial
+);
+
 ReactDOM.render(
-	<App />,
+	<Provider store={ store }>
+		<App />
+	</Provider>,
 	document.getElementById('root')
 );
