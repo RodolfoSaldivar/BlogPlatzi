0ad1a243a5fe40f57616a7af4733933b20e7cdf5
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2019-04-25 10:49:00 -0500

    stateless App

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,52 @@
 import React from 'react';
 
-const App = (props) => (
-	<div className="margen">
-		<table className="tabla">
-			<thead>
-				<th>
-					Nombre
-				</th>
-				<th>
-					Correo
-				</th>
-				<th>
-					Enlace
-				</th>
-			</thead>
-			<tbody>
-				<tr>
-					<td>
-						Rodolfo
-					</td>
-					<td>
-						Rodolfo@platzi.com
-					</td>
-					<td>
-						Rodolfo.com
-					</td>
-				</tr>
-				<tr>
-					<td>
-						Rodolfo
-					</td>
-					<td>
-						Rodolfo@platzi.com
-					</td>
-					<td>
-						Rodolfo.com
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-);
+const App = () => {
+
+	const ponerFilas = () => [
+		<tr>
+			<td>
+				Rodolfo
+			</td>
+			<td>
+				Rodolfo@platzi.com
+			</td>
+			<td>
+				Rodolfo.com
+			</td>
+		</tr>,
+		<tr>
+			<td>
+				Rodolfo
+			</td>
+			<td>
+				Rodolfo@platzi.com
+			</td>
+			<td>
+				Rodolfo.com
+			</td>
+		</tr>
+	];
+
+	return (
+		<div className="margen">
+			<table className="tabla">
+				<thead>
+					<th>
+						Nombre
+					</th>
+					<th>
+						Correo
+					</th>
+					<th>
+						Enlace
+					</th>
+				</thead>
+				<tbody>
+					{ ponerFilas() }
+				</tbody>
+			</table>
+		</div>
+	)
+};
 
 export default App;
