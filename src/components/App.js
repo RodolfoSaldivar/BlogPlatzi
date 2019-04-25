d9e7a395101bec9de01194c1919f7ee461383a11
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2019-04-25 10:29:27 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const App = (props) => (
+	<div className="margen">
+		<table className="tabla">
+			<thead>
+				<th>
+					Nombre
+				</th>
+				<th>
+					Correo
+				</th>
+				<th>
+					Enlace
+				</th>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						Rodolfo
+					</td>
+					<td>
+						Rodolfo@platzi.com
+					</td>
+					<td>
+						Rodolfo.com
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Rodolfo
+					</td>
+					<td>
+						Rodolfo@platzi.com
+					</td>
+					<td>
+						Rodolfo.com
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+);
+
+export default App;
