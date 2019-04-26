f21ec2f7c35e146fc3b7d69a604699b331eaa84c
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2019-04-25 22:44:11 -0500

    try catch

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 import { TRAER_TODOS } from '../types/usuariosTypes';
 
 export const traerTodos = () => async (dispatch) => {
-	const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
-	dispatch({
-		type: TRAER_TODOS,
-		payload: respuesta.data
-	})
+	try {
+		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+		dispatch({
+			type: TRAER_TODOS,
+			payload: respuesta.data
+		})
+	}
+	catch (error) {
+		console.log('Error: ', error.message);
+	}
 };
