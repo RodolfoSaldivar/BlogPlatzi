0824cf231f43b367f36ade6e7eee54e7d5357d84
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2019-04-26 17:27:50 -0500

    info usuarios en publicaciones

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as usuariosActions from '../../actions/usuariosActions';
 
 class Publicaciones extends Component {
+
+	componentDidMount() {
+		if (!this.props.usuarios.length) {
+			this.props.traerTodos();
+		}
+	}
+
 	render() {
+		console.log(this.props);
 		return (
 			<div>
+				<h1>
+					Publicaciones de 
+				</h1>
 				{ this.props.match.params.key }
 			</div>
 		);
 	}
 }
 
-export default Publicaciones;
+const mapStateToProps = (reducers) => {
+	return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps, usuariosActions)(Publicaciones);
