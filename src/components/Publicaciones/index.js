0024e3ceabbf5a77135006a2c157036d3aa05126
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2019-04-26 16:54:05 -0500

    publicaciones

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class index extends Component {
+class Publicaciones extends Component {
 	render() {
 		return (
 			<div>
@@ -10,4 +10,4 @@ class index extends Component {
 	}
 }
 
-export default index;
+export default Publicaciones;
